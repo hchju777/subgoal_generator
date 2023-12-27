ec717bc245d2549317bbe21dccd0b3c79220f4e1
Changju Hwang <hchju777@korea.ac.kr>
2023-12-27 22:36:08 +0900

    Initial setting

---
+++ build/CMakeFiles/subgoal_generator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/subgoal_generator.dir/src/main.cpp.o"
+  "subgoal_generator"
+  "subgoal_generator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/subgoal_generator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
